71044638f94c701c62594baba536aa3143b4a148
Laura Collins <laura.e.collins1@gmail.com>
2019-10-13 22:58:12 -0500

    completed

--- src/javascripts/helpers/data/planets.js
+++ src/javascripts/helpers/data/planets.js
@@ -1,8 +1,18 @@
 /* eslint-disable max-len */
+import mercuryImg from '../../assets/images/mercury.gif';
+import venusImg from '../../assets/images/venus.gif';
+import earthImage from '../../assets/images/earth.gif';
+import marsImage from '../../assets/images/mars.gif';
+import jupiterImage from '../../assets/images/jupiter.gif';
+import saturnImage from '../../assets/images/saturn.gif';
+import uranusImage from '../../assets/images/uranus.gif';
+import neptuneImage from '../../assets/images/neptune.gif';
+// import plutoImage from '../../assets/images/pluto.gif';
+
 const planets = [
   {
     name: 'Mercury',
-    imageUrl: '#',
+    imageUrl: `${mercuryImg}`,
     description: 'The smallest planet in our solar system and nearest to the Sun, Mercury is only slightly larger than Earth\'s Moon. From the surface of Mercury, the Sun would appear more than three times as large as it does when viewed from Earth, and the sunlight would be as much as seven times brighter. Despite its proximity to the Sun, Mercury is not the hottest planet in our solar system – that title belongs to nearby Venus, thanks to its dense atmosphere. Mercury\'s thin atmosphere, or exosphere, is composed mostly of oxygen (O2), sodium (Na), hydrogen (H2), helium (He), and potassium (K). Atoms that are blasted off the surface by the solar wind and micrometeoroid impacts create Mercury\'s exosphere.',
     isGasPlanet: false,
     numberOfMoons: 0,
@@ -10,7 +20,7 @@ const planets = [
   },
   {
     name: 'Venus',
-    imageUrl: '#',
+    imageUrl: `${venusImg}`,
     description: 'Second planet from the Sun and our closest planetary neighbor, Venus is similar in structure and size to Earth, but it is now a very different world. Venus spins slowly in the opposite direction most planets do. Its thick atmosphere traps heat in a runaway greenhouse effect, making it the hottest planet in our solar system—with surface temperatures hot enough to melt lead. Glimpses below the clouds reveal volcanoes and deformed mountains. One day on Venus lasts 243 Earth days because Venus spins backwards, with its sun rising in the west and setting in the east. The planet’s surface temperature is about 900 degrees Fahrenheit (465 degrees Celsius)—hot enough to melt lead.',
     isGasPlanet: false,
     numberOfMoons: 0,
@@ -18,7 +28,7 @@ const planets = [
   },
   {
     name: 'Earth',
-    imageUrl: '#',
+    imageUrl: `${earthImage}`,
     description: `Our home planet is the third planet from the Sun, and the only place we know of so far that’s inhabited by living things. While Earth is only the fifth largest planet in the solar system, it is the only world in our solar system with liquid water on the surface. Just slightly larger than nearby Venus, Earth is the biggest of the four planets closest to the Sun, all of which are made of rock and metal.
     
     The name Earth is at least 1,000 years old. All of the planets, except for Earth, were named after Greek and Roman gods and goddesses. However, the name Earth is a Germanic word, which simply means “the ground.”`,
@@ -28,7 +38,7 @@ const planets = [
   },
   {
     name: 'Mars',
-    imageUrl: '#',
+    imageUrl: `${marsImage}`,
     description: `The fourth planet from the Sun, Mars is a dusty, cold, desert world with a very thin atmosphere. This dynamic planet has seasons, polar ice caps and weather and canyons and extinct volacanoes, evidence of an even more active past.
     
     Mars is one of the most explored bodies in our solar system, and it's the only planet where we've sent rovers to roam the alien landscape. NASA currently has three spacecraft in orbit, one rover and one lander on the surface and another rover under construction here on Earth. India and ESA also have spacecraft in orbit above Mars.
@@ -40,7 +50,7 @@ const planets = [
   },
   {
     name: 'Jupiter',
-    imageUrl: '#',
+    imageUrl: `${jupiterImage}`,
     description: `Jupiter has a long history surprising scientists—all the way back to 1610 when Galileo Galilei found the first moons beyond Earth. That discovery changed the way we see the universe. Fifth in line from the Sun, Jupiter is, by far, the largest planet in the solar system – more than twice as massive as all the other planets combined.
     
     Jupiter's familiar stripes and swirls are actually cold, windy clouds of ammonia and water, floating in an atmosphere of hydrogen and helium. Jupiter’s iconic Great Red Spot is a giant storm bigger than Earth that has raged for hundreds of years.`,
@@ -50,7 +60,7 @@ const planets = [
   },
   {
     name: 'Saturn',
-    imageUrl: '#',
+    imageUrl: `${saturnImage}`,
     description: `Saturn is the sixth planet from the Sun and the second largest planet in our solar system. Adorned with thousands of beautiful ringlets, Saturn is unique among the planets. It is not the only planet to have rings—made of chunks of ice and rock—but none are as spectacular or as complicated as Saturn's.
     
     Like fellow gas giant Jupiter, Saturn is a massive ball made mostly of hydrogen and helium.`,
@@ -60,7 +70,7 @@ const planets = [
   },
   {
     name: 'Uranus',
-    imageUrl: '#',
+    imageUrl: `${uranusImage}`,
     description: 'The first planet found with the aid of a telescope, Uranus was discovered in 1781 by astronomer William Herschel, although he originally thought it was either a comet or a star. It was two years later that the object was universally accepted as a new planet, in part because of observations by astronomer Johann Elert Bode. Herschel tried unsuccessfully to name his discovery Georgium Sidus after King George III. Instead the planet was named for Uranus, the Greek god of the sky, as suggested by Johann Bode. Uranus has 13 known rings. The inner rings are narrow and dark and the outer rings are brightly colored.',
     isGasPlanet: true,
     numberOfMoons: 27,
@@ -68,7 +78,7 @@ const planets = [
   },
   {
     name: 'Neptune',
-    imageUrl: '#',
+    imageUrl: `${neptuneImage}`,
     description: 'Dark, cold and whipped by supersonic winds, ice giant Neptune is the eighth and most distant planet in our solar system. More than 30 times as far from the Sun as Earth, Neptune is the only planet in our solar system not visible to the naked eye and the first predicted by mathematics before its discovery. In 2011 Neptune completed its first 165-year orbit since its discovery in 1846. NASA\'s Voyager 2 is the only spacecraft to have visited Neptune up close. It flew past in 1989 on its way out of the solar system.',
     isGasPlanet: false,
     numberOfMoons: 13,
--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,11 @@
 import 'bootstrap';
 import '../styles/main.scss';
 import planetcards from './components/planet-cards/planetcards';
+// import planetimages from './components/planet-images/planetimages';
 
 const init = () => {
   planetcards.planetNameCard();
+//   planetimages.mouseOverEvent();
 };
 
 init();
